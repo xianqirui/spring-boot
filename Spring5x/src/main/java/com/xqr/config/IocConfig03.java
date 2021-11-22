8a86c559850c4cc021a71b5d30d378f740969310
xqr <xqr@xqr.com>
2021-11-22 10:16:54 +0800

    2.0

--- Spring5x/src/main/java/com/xqr/config/IocConfig03.java
+++ Spring5x/src/main/java/com/xqr/config/IocConfig03.java
@@ -1,5 +1,6 @@
 package com.xqr.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,9 +10,13 @@ import org.springframework.context.annotation.PropertySource;
 //PropertySource声明资源文件
 @PropertySource(value = {"classpath:jdbc.properties"})
 public class IocConfig03 {
+    @Value("${jdbc.driver}")
     private String driver;
+    @Value("${jdbc.url}")
     private String url;
+    @Value("$jdbc.username")
     private String name;
+    @Value("jdbc.passwd")
     private String pwd;
 
     public void test(){
