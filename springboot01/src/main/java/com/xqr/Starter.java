da4e3b0574119e135c99c7c1703f2c8ec237abec
xqr <xqr@xqr.com>
2021-11-22 12:32:47 +0800

    spring boot 设置关闭图标

--- springboot01/src/main/java/com/xqr/Starter.java
+++ springboot01/src/main/java/com/xqr/Starter.java
@@ -1,12 +1,22 @@
 package com.xqr;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* 设置banner图标，在banner文件中按
+* 设置图标关闭
+* */
 @SpringBootApplication
 public class Starter {
     public static void main(String[] args) {
-        SpringApplication.run(Starter.class);
+        //SpringApplication.run(Starter.class);
+
+        SpringApplication springApplication=new SpringApplication(Starter.class);
+        //设置图标关闭
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        //启动项目
+        springApplication.run();
 
     }
 }
