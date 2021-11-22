c170ece7310091d17b07710ac9763f4be8d86f19
xqr <xqr@xqr.com>
2021-11-22 14:40:07 +0800

    spring boot  freemarker使用

--- springboot01/src/main/java/com/xqr/Starter.java
+++ springboot01/src/main/java/com/xqr/Starter.java
@@ -1,17 +1,24 @@
 package com.xqr;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /*
 * 设置banner图标，在banner文件中按
 * 设置图标关闭
 * */
 @SpringBootApplication
 public class Starter {
+    //使用日志
+    //private static Logger logger= LoggerFactory.getLogger(Starter.class);
+
     public static void main(String[] args) {
         //SpringApplication.run(Starter.class);
-
+        //打印日志
+        //logger.error("项目正在启动");
         SpringApplication springApplication=new SpringApplication(Starter.class);
         //设置图标关闭
         springApplication.setBannerMode(Banner.Mode.OFF);
--- springboot01/src/main/java/com/xqr/controller/HelloController.java
+++ springboot01/src/main/java/com/xqr/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.xqr.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,11 @@ public class HelloController {
     @ResponseBody
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping("index")
+    public String index(HttpServletRequest request){
+        request.setAttribute("mgs","haohaohao");
+        return "index";
     }
 }
