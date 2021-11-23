89a2aed3eb5a023e26037e56a2f53dcaf05c9ba7
xqr <xqr@xqr.com>
2021-11-23 16:14:01 +0800

    @RestController 分页查询

---
+++ springboot_mybatis/src/main/java/com/xqr/springboot/query/UserQuery.java
@@ -0,0 +1,32 @@
+package com.xqr.springboot.query;
+//分页
+public class UserQuery {
+    private Integer pageNum=1;//当前第几页
+    private Integer pageSize=10;//每页显示记录数
+    //条件参数
+    private String userName;
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
