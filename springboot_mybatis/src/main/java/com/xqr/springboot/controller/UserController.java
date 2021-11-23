190d55d7d2ec4c4447d11b1b3e8ab2cdb48b983f
xqr <xqr@xqr.com>
2021-11-23 11:22:30 +0800

    @RestController 注解使用/ /返回json,可以不加@ResponseBody注解

--- springboot_mybatis/src/main/java/com/xqr/springboot/controller/UserController.java
+++ springboot_mybatis/src/main/java/com/xqr/springboot/controller/UserController.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@Controller
+//@Controller //可以视图，可以json
+@RestController//返回json,可以不加@ResponseBody注解
 public class UserController {
     @Resource
     private UserService userService;
     @GetMapping("user/{userName}")
-    @ResponseBody
+    //@ResponseBody
     public User findUser(@PathVariable String userName){
     return userService.findUser(userName);
     }
